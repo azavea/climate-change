15b8b4d4f5ad4ec2f33cf920aa4359e73a4804e7
Jenny Fung <jfung@azavea.com>
2016-03-21 15:17:26 -0400

    Display futurefeelslike data from s3

--- web/src/app/components/api/city.service.js
+++ web/src/app/components/api/city.service.js
@@ -5,6 +5,7 @@
     function City($http, $q, CityList, FeelsLikeStub, strFormat) {
 
         var INDICATOR_URL_TEMPLATE = 'https://s3.amazonaws.com/nex-climate-indicators/{0}--{1}.json';
+        var FUTURE_CITY_URL_TEMPLATE = 'https://s3.amazonaws.com/futurefeelslikeprojections/rcp85/{0}--{1}.json';
 
         var module = {
             indicators: indicators,
@@ -23,7 +24,12 @@
         }
 
         function feelslike(city, admin) {
-            return $q.resolve(FeelsLikeStub);
+            // UI only accepts 1 scenario, default to rcp85
+            var rcp85_url = strFormat(FUTURE_CITY_URL_TEMPLATE,
+                                [city.replace(/\s+/g, '+'), admin.replace(/\s+/g, '+')]);
+            return $http.get(rcp85_url).then(function (response) {
+                return response.data;
+            });
         }
 
         function list() {
